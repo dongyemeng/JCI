808899de590375528e1f6053a0387a1587bc6656
Dongye Meng <dmeng@cs.unc.edu>
2013-10-15 03:04:08 -0400

    added a method to add up all (term, its context vector) together

--- src/test.java
+++ src/test.java
@@ -1,7 +1,15 @@
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+import java.util.SortedSet;
 
 import jci.Article;
 import jci.CRAFT;
+import jci.ContextVector;
+import jci.Term;
 
 
 public class test {
@@ -17,13 +25,43 @@ public class test {
 		String dir = "C:/Users/Dongye/Dropbox/Phenoscape/CRAFT corpus/craft-1.0";
 		CRAFT myCRAFT = new CRAFT(dir);
 		List<String> ids = myCRAFT.getArticleIDs();
+		Map<Term, ContextVector> termAndContextVector = new HashMap<Term, ContextVector>();
 		for (String id : ids) {
 			Article myArticle = myCRAFT.getArticle(id);
 			myArticle.process(3);
+			termAndContextVectorAddition(termAndContextVector,
+					myArticle.getTermAndContextVector());
 		}
+		
+		SortedSet<Term> terms = new TreeSet<Term>(termAndContextVector.keySet());
+		for (Term term : terms) { 
+			   ContextVector value = termAndContextVector.get(term);
+			   System.out.println(term.toString());
+			   System.out.println(value.toString());
+			}
+		
+		
 		
 		
 
+	}
+	
+	public static void termAndContextVectorAddition(Map<Term, ContextVector> t, Map<Term, ContextVector> s) {
+		Iterator<Entry<Term, ContextVector>> iter = s.entrySet().iterator();
+		while (iter.hasNext()) {
+			Entry<Term, ContextVector> e = iter.next();
+			Term key = e.getKey();
+			ContextVector value = e.getValue();
+			
+			if (t.containsKey(key)) {
+				ContextVector oldValue = t.get(key);
+				oldValue.add(value);
+				t.put(key, oldValue);
+			}
+			else {
+				t.put(key, value);
+			}
+		}
 	}
 
 }
