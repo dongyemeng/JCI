be9906ca7235d25262fd2088328e4c42c58cbdf6
Dongye Meng <dmeng@cs.unc.edu>
2013-10-15 03:03:29 -0400

    override compareTo() method

--- src/jci/Term.java
+++ src/jci/Term.java
@@ -3,7 +3,7 @@ package jci;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-public class Term {
+public class Term implements Comparable<Term>{
 	private String ID;
 	private String name;
 	public Term(String id, String n) {
@@ -32,6 +32,23 @@ public class Term {
 		return new HashCodeBuilder(13, 37)
 		.append(this.ID)
 		.toHashCode();
+	}
+	
+	@Override
+	public String toString() {
+		String termString = "\n"
+				+ "Term:\n"
+				+ "\tID: " + this.ID + "\n"
+				+ "\tName: " + this.name + "\n" 
+				+ "\n";
+
+		return termString;
+	}
+
+
+	@Override
+	public int compareTo(Term t) {
+		return this.ID.compareTo(t.ID);
 	}
 	
 
